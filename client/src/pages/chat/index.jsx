85dec1626b0e01b82f266acf16b2a3a4d1d92980
krishpatel07 <krishdobariya07@gmail.com>
2025-03-08 10:23:44 +0530

    initial commit

---
+++ client/src/pages/chat/index.jsx
@@ -0,0 +1,28 @@
+import { useAppStore } from "@/store";
+import { useEffect } from "react"; 
+import { useNavigate } from "react-router-dom"; 
+import { toast } from "sonner"; 
+import EmptyChatContainer from "./empty-chat-container";
+import ContactsContainer from "./contacts-container";
+import ChatContainer from "./chat-container";
+
+const Chat = () => {
+const { userInfo,selectedChatType } = useAppStore();
+const navigate = useNavigate();
+useEffect(() => {
+if (!userInfo.profileSetup) {
+toast("Please setup profile to continue.");
+navigate("/profile");
+}
+}, [userInfo, navigate]);
+return (
+<div className="flex h-[100vh] text-white overflow-hidden">
+<ContactsContainer/>
+{selectedChatType === undefined ? (
+  <EmptyChatContainer/>
+  ) : (
+<ChatContainer/>
+)}
+</div>
+)};
+export default Chat;
